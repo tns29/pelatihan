4857593b9fb60ddc99cdf16976ea79dacd0a1794
yusufarya <aryaherby29nov2k@gmail.com>
2023-12-23 15:58:50 +0700

    change label footer

--- resources/views/admin-page/layouts/main_layout.blade.php
+++ resources/views/admin-page/layouts/main_layout.blade.php
@@ -68,8 +68,7 @@
         {{-- <aside class="control-sidebar control-sidebar-dark">
             <!-- Control sidebar content goes here -->
         </aside> --}}
-        <!-- /.control-sidebar -->
-    
+        <!-- /.control-sidebar --> 
     </div>
     <!-- ./wrapper -->
 
