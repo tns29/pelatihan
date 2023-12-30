3ec688f32ed3c84423be872b03cf25b20e1c95b0
yusufarya <aryaherby29nov2k@gmail.com>
2023-12-30 16:39:11 +0700

    modify length title

--- app/Http/Controllers/TrainingContentController.php
+++ app/Http/Controllers/TrainingContentController.php
@@ -55,7 +55,7 @@ class TrainingContentController extends Controller
     {
         $validatedData = $request->validate([
             'training_id'      => 'required',
-            'title'      => 'required|max:30',
+            'title'      => 'required|max:70',
             'image'     => 'image|file|max:1024'
         ]);
 
@@ -114,7 +114,7 @@ class TrainingContentController extends Controller
     {
         $validatedData = $request->validate([
             'training_id'      => 'required',
-            'title'      => 'required|max:30',
+            'title'      => 'required|max:70',
             'image'     => 'image|file|max:1024'
         ]);
 
