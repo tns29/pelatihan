e540a7465c1211ced42e4cf78f466d8709f0e90c
yusufarya <aryaherby29nov2k@gmail.com>
2024-01-03 00:04:44 +0700

    aria-label name

--- resources/views/user-page/layouts/navbar.blade.php
+++ resources/views/user-page/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary rounded-2">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><b>UPTD Latihan Kerja Dinas Tenaga Kerja Kab. Tangerang</b></a>
+    <a class="navbar-brand" href="/" aria-label="Brand Name"><b>UPTD Latihan Kerja Dinas Tenaga Kerja Kab. Tangerang</b></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
