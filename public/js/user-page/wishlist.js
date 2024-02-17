9b9d33ae83e5a53c13baa1aacfb57066e9238a46
yusufarya <aryaherby29nov2k@gmail.com>
2024-02-18 01:34:34 +0700

    modify print out card

--- public/js/user-page/wishlist.js
+++ public/js/user-page/wishlist.js
@@ -39,7 +39,7 @@ function printDiv() {
                 }
                 .content-body {
                     position: absolute;
-                    top: 33%;
+                    top: 34.5%;
                     left: 13%;
                     margin-top: 5px;
                     padding: 45px 55px;
