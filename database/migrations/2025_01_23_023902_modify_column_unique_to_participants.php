32226b103bc1c23e499107b2e4f0e0dd46f32f6e
yusufarya <aryaherby29nov2k@gmail.com>
2025-01-23 03:19:22 +0700

    drop username unique

---
+++ database/migrations/2025_01_23_023902_modify_column_unique_to_participants.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            // Remove the unique constraint
+            $table->dropUnique('users_username_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('participants', function (Blueprint $table) {
+            $table->unique('username', 'users_username_unique');
+        });
+    }
+};
